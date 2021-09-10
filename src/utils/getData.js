82780a419f1ed3d399c05fe9ca68513b54c9fe04
Miguel Aguilar <migueleduagui@gmail.com>
2021-09-10 18:32:29 -0300

    solucionado error de api

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,6 +1,5 @@
-//const API = process.env.API; //asi ocultamos nuestra api a traves de el arhvi .env pero lo comentaremos por ahora porque al momento de hacer deploy en netlify nos arroja un error.
+const API = process.env.API;
 
-const API = 'https://randomuser.me/api/'
 const getData = async (id) => {
   const apiURl = id ? `${API}${id}` : API;
   try {
